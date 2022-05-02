41b58202b572f3049c52f861fe8ad9408e0af9aa
yrkim <shim99887@naver.com>
2022-05-02 10:24:48 +0900

    config 정보 세팅

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require('./model/User')
+
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended : true}))
+
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yrkim:Thirautech1!@boilerplate.tpf4p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const {mongoUrl} = require("./config/dev");
+mongoose.connect(config.mongoUrl, {
     useNewUrlParser : true, useUnifiedTopology : true
 }).then(() => console.log('MongoDB Connected')
 ).catch(err =>  console.log(err))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!test')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err)
+            return res.json({success : false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
-// mongodb+srv://yrkim:<password>@boilerplate.tpf4p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
